4d00844a1ef86048870d53c6efdbdeab65c81436
ahmdlora <83436027+ahmadloraf@users.noreply.github.com>
2021-05-06 13:31:03 +0200

    Add files via upload

---
+++ New folder/mathClass.java
@@ -0,0 +1,12 @@
+public class mathClass {
+    public static void main(String[] args){
+      System.out.println(Math.PI);
+      System.out.println(Math.abs(3000.90));
+      System.out.println(Math.min(100, 200));
+      System.out.println(Math.min(100, 200));
+      System.out.println(Math.sqrt(340));
+      System.out.println(Math.pow(32, 34));
+
+        
+    } 
+}
